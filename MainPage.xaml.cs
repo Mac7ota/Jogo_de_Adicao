9a01882bccf1fee7c8efff821dc318e5dabbf122
Mauro Alves <mauro.alves.aguiar@outlook.com>
2023-12-26 14:12:45 -0300

    Aplicativo feito

---
+++ MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿namespace JogoAdicao
+{
+    public partial class MainPage : ContentPage
+    {
+        int score = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnZerar_Clicked(object sender, EventArgs e)
+        {
+            bool resposta = await DisplayAlert("Zerar", "Deseja zerar o placar?", "Sim", "Não");
+            if (resposta)
+            {
+                score = 0;
+                lbScore.Text = score.ToString();
+            }
+        }
+
+        private async void BtnJogar_Clicked(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int num1 = random.Next(0, 51);
+            int num2 = random.Next(0, 51);
+            int total = num1 + num2;
+
+            string texto = await DisplayPromptAsync("Jogo de Adição", $"{num1} + {num2} = ?");
+            try
+            {
+                                int resposta = Convert.ToInt32(texto);
+                if (resposta == total)
+                {
+                    score++;
+                    lbScore.Text = score.ToString();
+                    await DisplayAlert("Jogo de Adição", "Resposta correta!", "OK");
+                }
+                else
+                {
+                    await DisplayAlert("Jogo de Adição", "Resposta incorreta!", "OK");
+                }
+            }
+            catch (Exception)
+            {
+                await DisplayAlert("Jogo de Adição", "Resposta incorreta!", "OK");
+            }
+        }
+    }
+
+}
---
+++ obj/Debug/net8.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("JogoAdicao.MainApplication, JogoAdicao, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64143739fe7ff31656.MainApplication.class, crc64143739fe7ff31656.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
